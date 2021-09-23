0baf99eed621cba5112885dae30eba5740983628
Lakshay Akula <lakshayakula@gmail.com>
2021-09-23 12:40:43 -0400

    Configure EAS Build for iOS

--- with-splash-screen/app.config.js
+++ with-splash-screen/app.config.js
@@ -3,8 +3,11 @@ export default {
     "https://github.com/expo/expo/blob/master/templates/expo-template-blank/assets/icon.png?raw=true",
   splash: {
     image:
-      "https://github.com/expo/expo/blob/master/templates/expo-template-blank/assets/splash.png?raw=true",
+      "./assets/splash.png",
     resizeMode: "contain",
     backgroundColor: "#ffffff",
   },
+  ios: {
+    bundleIdentifier: "com.lakshayakula.withsplashscreen"
+  }
 };
